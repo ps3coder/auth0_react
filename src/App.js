dafad12bc865a682bf3e7c43ee3c7146c897cb05
ps3coder <ss559679035@gmail.com>
2024-10-23 21:46:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const { user, loginWithRedirect } = useAuth0();
+  console.log("Current User", user);
+  return <div>
+    <button onClick={(e) => loginWithRedirect()}>
+      Login with redirect
+    </button>
+  </div>;
+};
 
 export default App;
